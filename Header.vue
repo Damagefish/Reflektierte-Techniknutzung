4966675f2c8a8f5579533af4381f247ccd995637
NielsVagt <79412259+NielsVagt@users.noreply.github.com>
2024-01-05 12:53:46 +0100

    Add files via upload

---
+++ Header.vue
@@ -0,0 +1,30 @@
+<template>
+  <header>
+
+    <input placeholder="Suche..">
+
+    </header>
+</template>
+
+<script>
+
+    export default {
+        name: 'HeaderSection',
+    }
+
+</script>
+
+<style scoped>
+
+header {
+    background-color: yellow;
+    height: 150px;
+    padding: 16px;
+    display: flex;
+    align-items: end;
+    border-bottom-left-radius: 8px;
+    border-bottom-right-radius: 8px;
+  }
+</style>
+        
+    
