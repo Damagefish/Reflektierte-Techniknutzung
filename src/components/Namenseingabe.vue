30da2ca3c64868cc01fa424d0c8291695d3737d2
Mattias Holand <mattias.holand@student.uni-luebeck.de>
2024-01-05 14:26:22 +0100

    added Namenseingabe Screen

---
+++ src/components/Namenseingabe.vue
@@ -0,0 +1,23 @@
+<template>
+<head>
+    <meta charset="UTF-8">
+    <title>Namenseingabe</title>
+</head>
+
+<body>
+    <div>
+        <h4>Willkommen! Ich bin der Erzähler, der dich durch deine Zielsetzung begleitet</h4>
+        <h4>Bevor wir beginnen: Wie soll ich dich ansprechen?</h4>
+        <textarea>Wie soll ich dich ansprechen?</textarea> 
+        <font-awesome-icon :icon="['fas', 'arrow-right']" />
+    </div>
+</body>
+</template>
+
+<style scoped>
+
+.h4{
+    color: white;
+}
+
+</style>
