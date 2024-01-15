6813f810b7305de59c2961548851f6f8b2e79562
Mattias <116845975+Damagefish@users.noreply.github.com>
2024-01-15 01:12:22 +0100

    updated Namenseingabe Component

--- src/components/Namenseingabe.vue
+++ src/components/Namenseingabe.vue
@@ -1,23 +1,46 @@
 <template>
-<head>
-    <meta charset="UTF-8">
-    <title>Namenseingabe</title>
-</head>
 
-<body>
     <div>
         <h4>Willkommen! Ich bin der Erzähler, der dich durch deine Zielsetzung begleitet</h4>
         <h4>Bevor wir beginnen: Wie soll ich dich ansprechen?</h4>
-        <textarea>Wie soll ich dich ansprechen?</textarea> 
+        <input v-model="name" placeholder="Wie soll ich dich ansprechen?"/>
+        <p> Willkommen {{ name }}</p>
+        <div id="arrowRight">
         <font-awesome-icon :icon="['fas', 'arrow-right']" />
+        </div>
     </div>
-</body>
 </template>
+
+<script>
+import vue from 'vue-class-component'
+export default {
+    data() {
+        return {
+            name: ''
+        }
+    }
+}
+</script>
 
 <style scoped>
 
-.h4{
+h4{
+    color: black;
+}
+textarea{
+    background-color: brown;
     color: white;
+}
+input{
+    text-align: center;
+    width: 170px;
+}
+.name{
+    color: blue;
+}
+#arrowRight{
+    justify-content: right;
+    align-items: end;
 }
 
 </style>
