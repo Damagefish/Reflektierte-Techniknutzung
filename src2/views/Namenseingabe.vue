8bccb38f4d0016739bff4d51c420021c6bf4fbe3
Mattias <116845975+Damagefish@users.noreply.github.com>
2024-01-17 19:05:55 +0100

    added Namenseingabe to new src

---
+++ src2/views/Namenseingabe.vue
@@ -0,0 +1,44 @@
+<template>
+    <div>
+        <h4>Willkommen! Ich bin der Erzähler, der dich durch deine Zielsetzung begleitet</h4>
+        <h4>Bevor wir beginnen: Wie soll ich dich ansprechen?</h4>
+        <input v-model="name" placeholder="Wie soll ich dich ansprechen?"/>
+        <p> Willkommen {{ name }}</p>
+        <div id="arrowRight">
+        <font-awesome-icon :icon="['fas', 'arrow-right']" />
+        </div>
+    </div>
+</template>
+
+<script>
+import vue from 'vue-class-component'
+export default {
+    data() {
+        return {
+            name: ''
+        }
+    }
+}
+</script>
+
+<style scoped>
+
+h4{
+    color: black;
+}
+textarea{
+    background-color: brown;
+    color: white;
+}
+input{
+    text-align: center;
+    width: 170px;
+}
+.name{
+    color: blue;
+}
+#arrowRight{
+    justify-content: right;
+    align-items: end;
+}
+</style>
