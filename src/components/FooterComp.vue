05ec3fddbf914e1f278b9624a67a8dd64576b4b0
Mattias Holand <mattias.holand@student.uni-luebeck.de>
2024-01-17 19:17:03 +0100

    rename src2 to src

---
+++ src/components/FooterComp.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="footer">
+      <div class="footer-section">Teil 1</div>
+      <div class="footer-section">Teil 2</div>
+      <div class="footer-section">Teil 3</div>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: 'FooterComp',
+  };
+  </script>
+  
+  <style scoped>
+  .footer {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    background-color: #333;
+    color: white;
+    padding: 10px;
+  }
+  
+  .footer-section {
+    flex: 1;
+    text-align: center;
+  }
+  </style>
+  
---
+++ src/views/CheckIn2View.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <h1>Check In 2</h1>
+  </div>
+</template>
+
+<script>
+
+
+export default {
+  components: {
+    
+  }
+};
+</script>
+
+<style scoped>
+  h1 {
+    text-align: center;
+  }
+</style>
