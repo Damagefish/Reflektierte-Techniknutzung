738f82b9ede1b4967e15d4e270bc8973e3e94966
Mattias Holand <mattias.holand@student.uni-luebeck.de>
2024-01-28 15:18:44 +0100

    added Links to Footer

--- src/components/FooterComp.vue
+++ src/components/FooterComp.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="footer">
-    <div class="footer-section"> <font-awesome-icon :icon="['fas', 'chart-column']" /></div>
-    <div class="footer-section"> <font-awesome-icon :icon="['fas', 'house']" /></div>
-    <div class="footer-section"> <font-awesome-icon :icon="['fas', 'clock']" /></div>
+    <div class="footer-section"> <font-awesome-icon :icon="['fas', 'chart-column']" @click="$router.push('/barchart')"/></div>
+    <div class="footer-section"> <font-awesome-icon :icon="['fas', 'house']" @click="$router.push('/mainscreen')"/></div>
+    <div class="footer-section"> <font-awesome-icon :icon="['fas', 'clock']" @click="$router.push('/checkIn1View')"/></div>
   </div>
 </template>
 
