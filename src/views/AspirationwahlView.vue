674c4b892947080f68276e6d7802cb40b17198bf
Mattias Holand <mattias.holand@student.uni-luebeck.de>
2024-01-29 17:21:29 +0100

    fixed Tag selection

--- src/views/AspirationwahlView.vue
+++ src/views/AspirationwahlView.vue
@@ -4,7 +4,23 @@
     <h4>Überlege dir ein langfristiges Ziel, dass du erreichen möchtest und wähle den passenden Lebensbereich aus.</h4>
     <h4>Beschreibe außerdem kurz, wie dein aktueller Stand ist.</h4>
     <div id= "tags">
-       <selectedTags/>
+        <div class="lebensbereiche-comp">
+      <div class="frame2">
+        
+        <div class="tags-container">
+          <div
+          class="tag"
+          id = "tag"
+          v-for="tag in tags"
+          :key="tag"
+          @click="toggleTag(tag)"
+          :class="{ 'selected': tagsSelected.includes(tag)}"
+        >
+          {{ tag }}
+          </div>
+        </div>
+      </div>
+  </div>
     </div>
     <div id = "SollZustand">
         <textfield>
@@ -16,27 +32,24 @@
         <textfield>So sieht mein aktueller Stand aus:</textfield>
         <input v-model="istZustand"/>
     </div>
-    <font-awesome-icon :icon="['fas', 'circle-arrow-right']" @click="{setAspiration(); setIstZustand();$router.push('/meilensteine-waehlen') }" />
+    <font-awesome-icon :icon="['fas', 'circle-arrow-right']" @click="{setAspiration(); setIstZustand(); setAspirationTags(); $router.push('/meilensteine-waehlen') }" />
     
     </template>
     
     <script>
     import LebensbereicheComp from '@/components/LebensbereicheComp.vue';
     import HeaderComp from '@/components/HeaderComp'
-    import selectedTags from '@/components/SelectedTags.vue'
     export default{
     
         data(){
             return{
-                tags: [], 
                 aspiration: '', 
                 istZustand: '',
+                tags: this.$store.getters.getSelectedTags,
+                tagsSelected: []
             }
-    
         },
         components:{
-           // LebensbereicheComp,
-            selectedTags, 
             HeaderComp
         }, 
         methods:{
@@ -46,11 +59,64 @@
             setIstZustand(){
                 this.$store.commit('setIstZustand', this.istZustand);
             },
+            setAspirationTags(){
+                this.$store.commit('setAspirationTags', this.tagsSelected)
+            },
+            toggleTag(tag) {
+                 if (this.tagsSelected.includes(tag)) {
+                    this.tagsSelected = this.tagsSelected.filter((selectedTag) => selectedTag !== tag);
+                } else {
+                    this.tagsSelected.push(tag);
+                }     
+            },
         }
     }
     </script>
     
     <style>
-    
-    
+    .lebensbereiche-comp {
+    width: 50%;
+    margin: auto;
+    }
+  
+    .frame {
+    padding: 20px;
+    border: 1px solid #ddd;
+    border-radius: 15px;
+    margin-bottom: 5px;
+    }
+  
+    .frame2 {
+    padding: 20px;
+    border: 1px solid #ddd;
+    border-radius: 15px;
+    margin-bottom: 5px;
+    }
+  
+    .frame3{
+    padding: 20px;
+    border: 1px solid #ddd;
+    border-radius: 15px;
+    }
+    .tag {
+    display: inline-block;
+    padding: 8px 12px;
+    margin-right: 10px;
+    margin-bottom: 10px;
+    border: 1px solid #d3d3d3;
+    border-radius: 8px;
+    background-color: #e0e0e0;
+    cursor: pointer;
+    transition: background-color 0.3s ease-in-out, box-shadow 0.3s ease-in-out;
+    }
+  
+    .tag:hover {
+    background-color: #c0c0c0;
+    }
+  
+    .tag.selected {
+    background-color: #4caf50;
+    color: #fff;
+    box-shadow: inset 5px 5px 10px #357a38, inset -5px -5px 10px #61bd61;
+    }
     </style>
