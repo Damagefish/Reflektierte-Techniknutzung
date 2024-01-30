db20585126e91ecdad27a0a1c544aa30c83c2ea2
Mattias Holand <mattias.holand@student.uni-luebeck.de>
2024-01-30 18:54:19 +0100

    added Roboto font to App.vue

--- src/App.vue
+++ src/App.vue
@@ -33,7 +33,7 @@ export default {
   background-color: #494448;
 }
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: "Roboto";
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
