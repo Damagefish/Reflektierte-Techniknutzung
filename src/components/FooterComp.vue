9bbb65a001d09017c8bdcc7cadf9eba708dae76f
NielsVagt <nielsvagt@web.de>
2024-01-30 13:24:11 +0100

    footer design

--- src/components/FooterComp.vue
+++ src/components/FooterComp.vue
@@ -1,8 +1,14 @@
 <template>
   <div class="footer">
-    <div class="footer-section"> <font-awesome-icon :icon="['fas', 'calendar']" @click="$router.push('/meilensteine-waehlen')"/></div>
-    <div class="footer-section"> <font-awesome-icon :icon="['fas', 'house']" @click="$router.push('/mainscreen')"/></div>
-    <div class="footer-section"> <font-awesome-icon :icon="['fas', 'clock']" @click="$router.push('/checkIn1View')"/></div>
+    <div class="footer-section">
+      <font-awesome-icon :icon="['fas', 'calendar']" @click="$router.push('/meilensteine-waehlen')"/>
+    </div>
+    <div class="footer-section">
+      <font-awesome-icon :icon="['fas', 'house']" @click="$router.push('/mainscreen')"/>
+    </div>
+    <div class="footer-section">
+      <font-awesome-icon :icon="['fas', 'clock']" @click="$router.push('/checkIn1View')"/>
+    </div>
   </div>
 </template>
 
@@ -20,10 +26,18 @@ export default {
   background-color: #333;
   color: white;
   padding: 10px;
+  border: 2px solid #555; 
+  border-radius: 8px; 
 }
 
 .footer-section {
   flex: 1;
   text-align: center;
+  padding: 8px; 
+  border-right: 1px solid #555; 
+}
+
+.footer-section:last-child {
+  border-right: none; 
 }
 </style>
